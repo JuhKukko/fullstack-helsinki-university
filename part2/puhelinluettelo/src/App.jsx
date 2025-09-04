bd264ec97136e2c6d0862b5352e63a3d568c33cb
Juhani Kukko <juhani.kukko@procompltd.com>
2025-09-04 17:23:05 +0300

    Solution for part 2 task 2.11

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ searchWord, handleSearchInputChange }) => {
   return(
@@ -35,16 +36,19 @@ const Persons = ({ persons }) => {
 }
 
 const App = () => {
- const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+ const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
   const [searchWord, setSearchWord] = useState('')
   
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const addPerson = (formEvent) => {
     formEvent.preventDefault();
     let found = persons.find((p) => p.name === newName)
