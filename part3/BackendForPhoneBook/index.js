c6c08ff1b125128b48595a777f4c337c746c4122
Juhani Kukko <juhani.kukko@procompltd.com>
2025-09-10 02:01:38 +0300

    Solution for part 3 tasks 3.1 - 3.8 (node backend for phone book)

---
+++ part3/BackendForPhoneBook/index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+
+app.use(express.json())
+
+app.use(morgan('tiny'))
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": "1"
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "234234234",
+    "id": "2"
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": "3"
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "1231-123123",
+    "id": "4"
+  }
+]
+
+app.get('/info', (request, response) => {
+  response.send('<p> Phonebook has info for ' + persons.length + ' people.</p><br> ' + new Date().toLocaleString())
+})
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.post('/api/persons', (request, response) => {
+  
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json ({ error: 'name must be unique' }) 
+  }
+
+  const personId = String(getRandomIntInclusive(100, 50000))
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: personId,
+  }
+
+  persons = persons.concat(person)
+  console.log(person)
+  response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3002
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
