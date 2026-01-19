5dfe897db50e3837801fe6f8acc2ae38b5bdec77
Juhani Kukko <juhkukko@gmail.com>
2026-01-19 13:33:30 +0200

    Tasks 3.15 & 3.16

--- part3/BackendForPhoneBook/index.js
+++ part3/BackendForPhoneBook/index.js
@@ -36,24 +36,32 @@ let persons = [
     "number": "1231-123123",
     "id": "4"
   }
-]
+] 
 
-app.get('/info', (request, response) => {
+// Info (array)
+/* app.get('/info', (request, response) => {
   response.send('<p> Phonebook has info for ' + persons.length + ' people.</p><br> ' + new Date().toLocaleString())
+}) */
+
+// Info (mongodb)
+app.get('/info', (request, response) => {
+  Person.find({}).then(personsdb => {
+    let count = personsdb.length;
+    response.send('<p> Phonebook has info for ' + count + ' people.</p><br> ' + new Date().toLocaleString())
+  })
 })
 
 // GET all persons
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
+  Person.find({})
+  .then(persons => {
     response.json(persons)
   })
 })
 
 // POST a new person
-app.post('/api/persons', (request, response) => {
-  
+app.post('/api/persons', (request, response) => {  
   const body = request.body
-
   if (!body.name) {
     return response.status(400).json({ 
       error: 'name missing' 
@@ -66,9 +74,10 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
+  /*
   if (persons.find(p => p.name === body.name)) {
     return response.status(400).json ({ error: 'name must be unique' }) 
-  }
+  } */
 
   const person = new Person({
     name: body.name,
@@ -83,20 +92,54 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+// PUT update person data
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name;
+      person.number = number;
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
 // GET a person by id
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person
   .findById(request.params.id)
   .then(person => {
-    response.json(person)
+    if(person) {
+      response.json(person)
+    } else {
+      response.status(404).end();
+    }    
   })
+  .catch(error => next(error))  
 })
 
-// DELETE a person by id
-app.delete('/api/persons/:id', (request, response) => {
+// DELETE a person by id (array)
+/* app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
+}) */
+
+// DELETE a person by id (mongodb)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -104,6 +147,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3002
 
